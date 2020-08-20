6e825e7c70e86ceafd1cc7ed28348416efd39906
Mathis Cavalli <52377212+MathisCpp@users.noreply.github.com>
2020-08-20 08:35:00 +0200

    Add files via upload

--- Minifier/Source.cpp
+++ Minifier/Source.cpp
@@ -1,5 +1,5 @@
 #include <Windows.h>
-#include "utils.h"
+#include "..\..\..\..\Desktop\Mathis\Code\headers\utils.h"
 
 #define SYNTHAX_TYPE_HTML 1
 #define SYNTHAX_TYPE_CSS 2
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
 	SetConsoleOutputCP(CP_UTF7);
 
 	if (argc != 6) {
-		puts("[html/css/js] -in \"Fichier Ã  optimiser.css\" -out \"Fichier optimisÃ©.css\"");
+		puts("[html/css/js] -in \"Fichier à optimiser.css\" -out \"Fichier optimisé.css\"");
 		Exit(1);
 	}
 
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]) {
 		synthaxType = SYNTHAX_TYPE_JS;
 	}
 	else {
-		puts("Langage non spÃ©cifiÃ©.");
+		puts("Langage non spécifié.");
 		Exit(-1);
 	}
 
@@ -59,82 +59,125 @@ int main(int argc, char* argv[]) {
 	HANDLE hInputFile = CreateFileA(szInputFile, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, NULL, NULL);
 	if (hInputFile == INVALID_HANDLE_VALUE) {
 		GetErrorString(szInputFile);
-		printf("Erreur lors de l'ouverture du fichier Ã  optimiser: %s\n", szInputFile);
+		printf("Erreur lors de l'ouverture du fichier à optimiser: %s\n", szInputFile);
 		Exit(2);
 	}
 
 	HANDLE hOutputFile = CreateFileA(szOutputFile, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, NULL, NULL);
 	if (hOutputFile == INVALID_HANDLE_VALUE) {
 		GetErrorString(szInputFile);
-		printf("Erreur lors de la crÃ©ation du fichier optimisÃ©: %s\n", szInputFile);
+		printf("Erreur lors de la création du fichier optimisé: %s\n", szInputFile);
 		Exit(2);
 	}
 
 	LARGE_INTEGER li;
 	GetFileSizeEx(hInputFile, &li);
 	if (li.QuadPart > 104857600) {
-		puts("La taille du fichier source ne doit pas dÃ©passer 100Mo.");
+		puts("La taille du fichier source ne doit pas dépasser 100Mo.");
 		Exit(3);
 	}
 	DWORD dw, dwFileSize = li.QuadPart;
 	LPSTR lpFileBuffer = (LPSTR)malloc(dwFileSize);
 	if (lpFileBuffer == NULL) {
-		puts("Erreur lors de l'allocation de la mÃ©moire.");
+		puts("Erreur lors de l'allocation de la mémoire.");
 		Exit(4);
 	}
 	if (!ReadFile(hInputFile, lpFileBuffer, dwFileSize, &dw, NULL)) {
 		GetErrorString(szInputFile);
-		printf("Erreur lors de la lecture du fichier Ã  optimiser: %s\n", szInputFile);
+		printf("Erreur lors de la lecture du fichier à optimiser: %s\n", szInputFile);
 		Exit(5);
 	}
 	LPSTR lpOutFileBuffer = (LPSTR)malloc(dwFileSize);
 	if (lpOutFileBuffer == NULL) {
-		puts("Erreur lors de l'allocation de la mÃ©moire.");
+		puts("Erreur lors de l'allocation de la mémoire.");
 		Exit(6);
 	}
 	ReplaceAllChars(lpFileBuffer, '\t', ' ');
 	DWORD dwOutIndex = 0;
-
+	char lastChar;		// Garde une trace du dernier caractère écrit
 
 	for (DWORD i = 0; i < dwFileSize; i++) {
 		if (lpFileBuffer[i] != '\n' && lpFileBuffer[i] != '\r') {
 			switch (synthaxType) {
 			case SYNTHAX_TYPE_HTML: {
 
+				lpOutFileBuffer[dwOutIndex] = lpFileBuffer[i];
+				lastChar = lpOutFileBuffer[dwOutIndex];
+				dwOutIndex++;
+
+
 				break;
 			}
+		parseCSS:
 			case SYNTHAX_TYPE_CSS: {
-				if (lpFileBuffer[i] == ' ') {
-					DWORD dwSpaces = 0;
-					while (lpFileBuffer[i + dwSpaces] == ' ')
+
+				
+				if (IsBadChar(lpFileBuffer[i])) {
+					DWORD dwSpaces = 1;
+					while (IsBadChar(lpFileBuffer[i + dwSpaces]))
 						dwSpaces++;
-					if (lpFileBuffer[i + dwSpaces] == '{')	// Supprime les espaces avant les '{'
+					register char after = lpFileBuffer[i + dwSpaces];
+					if (after == '{' || after == ':' || after == ';' || after == ',' || after == '/') {		// Supprime les espaces avant
 						i += dwSpaces;
+					}
+					else if (dwSpaces > 1) {	// Remplace les suites de plusieurs espaces par un seul
+						i += dwSpaces - 1;
+						lpFileBuffer[i] = ' ';
+					}
 				}
-				lpOutFileBuffer[dwOutIndex++] = lpFileBuffer[i];
 
-				if (lpFileBuffer[i] == '{' || lpFileBuffer[i] == '}' || lpFileBuffer[i] == ':' || lpFileBuffer[i] == ';' || lpFileBuffer[i] == ',') {
 
-					while (IsBadChar(lpFileBuffer[i + 1])) i++;
-					
-					if (lpFileBuffer[i + 1] == '/' && lpFileBuffer[i + 2] == '*') {
-						i += 3;
+				if (lpFileBuffer[i] == '/') {
+
+					if (lpFileBuffer[i + 1] == '*') {
+						i += 2;
 						while (lpFileBuffer[i] != '*' || lpFileBuffer[i + 1] != '/')	// Skip les commentaires
 							i++;
-						i++;
-						break;
+						i += 2;
+						while (IsBadChar(lpFileBuffer[i])) i++;
+						goto parseCSS;
 					}
-
-					if (lpFileBuffer[i + 1] == '}' && lpOutFileBuffer[dwOutIndex - 1] == ';') {
-						dwOutIndex--;
-						break;
-					}
-
 				}
+
+
+				if (lpFileBuffer[i] == '}') {
+					if (lastChar == ';') {
+						dwOutIndex--;
+					}
+					else if (lastChar == '{') {
+						i++;
+						while (lpOutFileBuffer[dwOutIndex != 0 ? dwOutIndex - 1 : 0] != '}' && dwOutIndex != 0) {	// Supprime les règles vides
+							dwOutIndex--;
+						}
+						goto parseCSS;
+					}
+				}
+
+
+				lpOutFileBuffer[dwOutIndex] = lpFileBuffer[i];
+				lastChar = lpOutFileBuffer[dwOutIndex];
+				dwOutIndex++;
+
+				if (lpFileBuffer[i] == '{' || lpFileBuffer[i] == '}' || lpFileBuffer[i] == ':' || lpFileBuffer[i] == ';' || lpFileBuffer[i] == ',' || lpFileBuffer[i] == '/') {
+					while (IsBadChar(lpFileBuffer[i + 1])) i++;
+					/*if (lpFileBuffer[i] == '/' && lpFileBuffer[i + 1] == '*') {
+						i += 2;
+						while (lpFileBuffer[i] != '*' || lpFileBuffer[i + 1] != '/')	// Skip les commentaires
+							i++;
+						i += 2;
+						while (IsBadChar(lpFileBuffer[i + 1])) i++;
+						
+					}*/
+				}
+
 				
 				break;
 			}
 			case SYNTHAX_TYPE_JS: {
+
+				lpOutFileBuffer[dwOutIndex] = lpFileBuffer[i];
+				lastChar = lpOutFileBuffer[dwOutIndex];
+				dwOutIndex++;
 
 				break;
 			}
@@ -144,9 +187,15 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	if (!WriteFile(hOutputFile, lpOutFileBuffer, dwOutIndex, &dw, NULL)) {
+	LPSTR lpOutFilePtr = lpOutFileBuffer;
+	if (*lpOutFileBuffer == ' ') {	// Supprime l'espace au début du fichier
+		lpOutFilePtr++;
+		dwOutIndex--;
+	}
+
+	if (!WriteFile(hOutputFile, lpOutFilePtr, dwOutIndex, &dw, NULL)) {
 		GetErrorString(szInputFile);
-		printf("Erreur lors de la lecture du fichier Ã  optimiser: %s\n", szInputFile);
+		printf("Erreur lors de la lecture du fichier à optimiser: %s\n", szInputFile);
 		Exit(7);
 	}
 
